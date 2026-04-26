4c207eb5b07d1d2e85547bba952fd93595f9d39f
518luck <165388484+518luck@users.noreply.github.com>
2026-04-27 00:20:16 +0800

    重构：将头部组件重命名为 app-header

---
+++ src/widgets/app-header/ui/app-header.tsx
@@ -0,0 +1,23 @@
+export function AppHeader() {
+  return (
+    <header>
+      <div>
+        <div>布局伸缩</div>
+        <div>间隔符号 separator</div>
+        <div>面包屑导航</div>
+      </div>
+
+      <div>
+        <div>github图标</div>
+        <div>
+          <div>搜索框</div>
+        </div>
+        <div>黑白主题切换</div>
+        <div>
+          <div>界面主题切换</div>
+        </div>
+        <div>信息框</div>
+      </div>
+    </header>
+  );
+}
