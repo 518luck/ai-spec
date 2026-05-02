ab92880c94dd4066a0de22f5cc4d1018fc32c3c8
518luck <165388484+518luck@users.noreply.github.com>
2026-05-02 17:39:14 +0800

    完善主题切换与快捷键提示

--- src/widgets/app-header/ui/theme-mode-toggle.tsx
+++ src/widgets/app-header/ui/theme-mode-toggle.tsx
@@ -1,3 +1,52 @@
+import { Button } from "@/shared/ui/button";
+import { Icons } from "@/shared/ui/icons";
+import { Kbd } from "@/shared/ui/kbd";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/shared/ui/tooltip";
+import { useTheme } from "next-themes";
+import { useCallback } from "react";
+
 export const ThemeModeToggle = () => {
-  return <div>ThemeModeToggle</div>;
+  const { setTheme, resolvedTheme } = useTheme();
+
+  const handleThemeToggle = useCallback(
+    (e?: React.MouseEvent) => {
+      const newMode = resolvedTheme === "dark" ? "light" : "dark";
+      const root = document.documentElement;
+
+      if (!document.startViewTransition) {
+        setTheme(newMode);
+        return;
+      }
+
+      if (e) {
+        root.style.setProperty("--x", `${e.clientX}px`);
+        root.style.setProperty("--y", `${e.clientY}px`);
+      }
+
+      document.startViewTransition(() => {
+        setTheme(newMode);
+      });
+    },
+    [resolvedTheme, setTheme],
+  );
+
+  const trigger = (
+    <Button
+      variant="secondary"
+      size={"icon"}
+      onClick={handleThemeToggle}
+      className="select-none"
+    >
+      <Icons.brightness />
+    </Button>
+  );
+
+  return (
+    <Tooltip>
+      <TooltipTrigger render={trigger} />
+      <TooltipContent>
+        主题切换 <Kbd>T</Kbd>
+      </TooltipContent>
+    </Tooltip>
+  );
 };
