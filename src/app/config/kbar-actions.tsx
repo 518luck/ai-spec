2a572cba5015346b517901bb5024ac51efde6e4f
zhangqingbo <zhangqingbo@yugeeker.com>
2026-05-03 13:21:57 +0800

    调整主题切换实现

--- src/app/config/kbar-actions.tsx
+++ src/app/config/kbar-actions.tsx
@@ -1,25 +1,44 @@
 "use client";
 
+import { THEMES } from "@/shared/configs/theme.config";
 import { useRegisterActions } from "kbar";
 import { useTheme } from "next-themes";
+import { useActiveTheme } from "@/shared/providers/active-theme-providers";
 
 function useKBarActions() {
   const { resolvedTheme, setTheme } = useTheme();
+  const { activeTheme, setActiveTheme } = useActiveTheme();
+
+  const cycleTheme = () => {
+    const currentIndex = THEMES.findIndex(
+      (theme) => theme.value === activeTheme,
+    );
+    const nextIndex = (currentIndex + 1) % THEMES.length;
+    setActiveTheme(THEMES[nextIndex].value);
+  };
 
   const actions = [
     {
-      id: "toggle-theme",
-      name: "切换主题",
-      shortcut: ["t"],
+      id: "toggle-light-dark-mode",
+      name: "切换暗/亮模式",
+      shortcut: ["d", "d"],
       section: "主题",
       keywords: "theme dark light",
       perform: () => {
         setTheme(resolvedTheme === "dark" ? "light" : "dark");
       },
     },
+    {
+      id: "cycle-theme",
+      name: "循环切换主题",
+      shortcut: ["t", "t"],
+      section: "主题",
+      keywords: "theme dark light",
+      perform: cycleTheme,
+    },
   ];
 
-  useRegisterActions(actions, [resolvedTheme]);
+  useRegisterActions(actions, [resolvedTheme, activeTheme]);
 }
 
 export { useKBarActions };
