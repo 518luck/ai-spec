38bead580e1660985752cbc7e288ed879d78ceba
zhangqingbo <zhangqingbo@yugeeker.com>
2026-05-09 22:41:52 +0800

    refactor: 将仪表板路由和布局移动到提示文件路径前缀下

--- app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardLayout } from "@/app/layouts/dashboard-layout";
-
-export default function dashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
