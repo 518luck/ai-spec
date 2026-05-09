43c5cb6bdc87b8a18356888bbb7279aac5fc1ad3
518luck <165388484+518luck@users.noreply.github.com>
2026-05-09 12:29:01 +0800

    配置 Credentials 邮箱密码登录认证提供者

--- src/shared/lib/auth/options.ts
+++ src/shared/lib/auth/options.ts
@@ -1,6 +1,7 @@
 import type { NextAuthConfig } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/prisma/index";
+import Credentials from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthConfig = {
   adapter: PrismaAdapter(prisma),
@@ -9,5 +10,36 @@ export const authOptions: NextAuthConfig = {
     maxAge: 30 * 24 * 60 * 60, // 默认 session 最大生命周期是30天, 30天不活跃自动登出
     updateAge: 24 * 60 * 60, //  24小时刷新一次 session
   },
-  providers: [],
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "prompt-shelf",
+      type: "credentials",
+      credentials: {
+        email: {
+          label: "邮箱",
+          type: "email",
+          // placeholder: "请输入邮箱",
+        },
+        password: {
+          label: "密码",
+          type: "password",
+          // placeholder: "请输入密码",
+        },
+      },
+      // credentials 代表上面credentials 写的配置项,用户传递的数据
+      // req 代表这次调用 authorize 的请求上下文
+      async authorize(credentials, req) {
+        if (!credentials) {
+          throw new Error("no-credentials");
+        }
+
+        const { email, password } = credentials;
+
+        if (!email || !password) {
+          throw new Error("no-credentials");
+        }
+      },
+    }),
+  ],
 };
