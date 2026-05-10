60aeec080c79c6c99b3c889e3ff21eeb03bb3b59
518luck <165388484+518luck@users.noreply.github.com>
2026-05-10 16:38:29 +0800

    refactor: 将 InteractiveGridPattern 组件迁移至认证布局并从注册页面中删除

--- src/pages/spec/auth/register/ui/page.tsx
+++ src/pages/spec/auth/register/ui/page.tsx
@@ -1,12 +1,9 @@
-import { InteractiveGridPattern } from "./Interactive-grid-pattern";
 import LoginForm from "./login-form";
 
 export default function RegisterPage() {
   return (
     <div>
-      <InteractiveGridPattern>
-        <LoginForm />
-      </InteractiveGridPattern>
+      <LoginForm />
     </div>
   );
 }
