517cfb64a6da299eaac611d798acf49ef69223f7
518luck <165388484+518luck@users.noreply.github.com>
2026-05-11 00:03:20 +0800

    refactor: 更新认证布局结构和样式以优化响应式表现和对齐方式

--- src/widgets/auth-form-layout/ui/layout.tsx
+++ src/widgets/auth-form-layout/ui/layout.tsx
@@ -5,27 +5,23 @@ export default function AuthFormLayout({
   showTerms = "app",
 }: PropsWithChildren<{ showTerms?: "app" | "partner" }>) {
   return (
-    <div className="flex h-full w-full min-w-0 flex-col">
-      <div className="grow basis-0">
-        <div className="h-24" />
+    <div className="flex h-full w-full max-w-sm min-w-0 flex-col">
+      <div className="h-8 md:h-12" />
+      <div className="flex flex-1 items-center">
+        <div className="w-full">{children}</div>
       </div>
-
-      <div className="relative flex w-full grow flex-col items-center justify-center">
-        {children}
-      </div>
-
-      <div className="flex grow basis-0 flex-col justify-end">
+      <div className="pt-8">
         {showTerms && (
           <p className="px-20 py-8 text-center text-xs font-medium text-neutral-500 md:px-0">
-            By continuing, you agree to Dub&rsquo;s
+            By continuing, you agree to Dub&rsquo;s{" "}
             <a
               href={`https://dub.co/legal/${showTerms === "app" ? "terms" : "partners"}`}
               target="_blank"
               className="font-semibold text-neutral-600 hover:text-neutral-800"
             >
               {showTerms === "app" ? "Terms of Service" : "Partner Terms"}
-            </a>
-            and
+            </a>{" "}
+            and{" "}
             <a
               href="https://dub.co/legal/privacy"
               target="_blank"
