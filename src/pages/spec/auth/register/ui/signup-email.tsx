0414cf470e5b48968ccb3b2cc702133cc71d87de
zhangqingbo <zhangqingbo@yugeeker.com>
2026-05-16 13:53:13 +0800

    功能: 新增 FieldError 组件并在注册表单中实现密码字段的条件渲染

--- src/pages/spec/auth/register/ui/signup-email.tsx
+++ src/pages/spec/auth/register/ui/signup-email.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSet,
+  FieldError,
 } from "@/shared/ui/field";
 import { Input } from "@/shared/ui/input";
 import { useForm } from "react-hook-form";
 import { useMediaQuery } from "@/shared/hooks/use-media-query";
+import { useState } from "react";
+import { Button } from "@/shared/ui/button";
 
 export function SignUpEmail() {
   const {
@@ -16,26 +21,52 @@ export function SignUpEmail() {
     formState: { errors },
   } = useForm();
 
+  const [showPassword, setShowPassword] = useState(false);
+
   const { isMobile } = useMediaQuery();
 
   return (
     <form>
       <FieldSet>
         <FieldGroup>
-          <Field>
-            <FieldLabel>
-              <FieldDescription>邮箱</FieldDescription>
-              <Input
-                type="email"
-                placeholder="panic@thedis.co"
-                autoComplete="email"
-                required
-                autoFocus={!isMobile}
-              />
-            </FieldLabel>
+          <Field data-invalid={!!errors?.email}>
+            <FieldLabel>邮箱</FieldLabel>
+            <Input
+              type="email"
+              placeholder="panic@thedis.co"
+              autoComplete="email"
+              required
+              autoFocus={!isMobile && showPassword}
+              {...register("email")}
+              aria-invalid={!!errors?.email}
+            />
+            <FieldError errors={[errors.email]} />
           </Field>
+
+          {showPassword && (
+            <Field data-invalid={!!errors?.password}>
+              <FieldLabel>密码</FieldLabel>
+              <Input
+                type="password"
+                placeholder="password"
+                autoComplete="password"
+                required
+                autoFocus={!isMobile && showPassword}
+                {...register("password")}
+                aria-invalid={!!errors?.password}
+              />
+              <FieldError errors={[errors.password]} />
+            </Field>
+          )}
         </FieldGroup>
       </FieldSet>
+
+      <Button
+        type="submit"
+        // text={isPending ? "Submitting..." : "Sign Up"}
+        // disabled={isPending}
+        // loading={isPending}
+      />
     </form>
   );
 }
