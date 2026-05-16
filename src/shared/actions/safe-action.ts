4c60463f5090b4a65288794dfe2c67a58fe338f8
518luck <165388484+518luck@users.noreply.github.com>
2026-05-16 15:50:23 +0800

    功能: 将 Axiom 日志集成到 safe action 的服务端错误处理程序中

--- src/shared/actions/safe-action.ts
+++ src/shared/actions/safe-action.ts
@@ -1,7 +1,12 @@
 import { createSafeActionClient } from "next-safe-action";
+import { logger } from "../lib/axiom/server";
+import { after } from "next/server";
 
 export const actionClient = createSafeActionClient({
   handleServerError: async (e) => {
+    logger.error(e.message, e);
+    after(logger.flush());
+
     if (e instanceof Error) {
       return e.message;
     }
