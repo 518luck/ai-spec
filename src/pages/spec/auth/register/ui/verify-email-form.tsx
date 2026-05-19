c1ee0c199b9c72287b0518f206d258bb6d500265
518luck <165388484+518luck@users.noreply.github.com>
2026-05-19 23:04:15 +0800

    feat: 使用 InputOTP 组件实现邮箱验证流程

---
+++ src/pages/spec/auth/register/ui/verify-email-form.tsx
@@ -0,0 +1,31 @@
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/shared/ui/input-otp";
+import { useState } from "react";
+
+export function VerifyEmailForm() {
+  const [value, setValue] = useState("");
+  return (
+    <div className="flex flex-col items-center">
+      <InputOTP
+        maxLength={6}
+        value={value}
+        onChange={(value) => setValue(value)}
+      >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+      <div className="text-center text-sm">
+        {value === "" ? (
+          <>Enter your one-time password.</>
+        ) : (
+          <>You entered: {value}</>
+        )}
+      </div>
+    </div>
+  );
+}
