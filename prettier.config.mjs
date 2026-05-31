f5e476c2833e7d7397438fda009980fa298b5331
518luck <165388484+518luck@users.noreply.github.com>
2026-06-01 01:46:18 +0800

    feat: 添加站点图标

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "**/*.svg",
+      options: {
+        parser: "html",
+      },
+    },
+  ],
 };
 
 export default config;
--- src/app/config/metadata.ts
+++ src/app/config/metadata.ts
@@ -4,4 +4,13 @@ export const metadata: Metadata = {
   title: "AI Spes",
   description:
     "A Next.js App Router starter organized with Feature-Sliced Design.",
+  icons: {
+    icon: [
+      {
+        url: "/crab_bookmark_glasses_centered_transparent.svg",
+        type: "image/svg+xml",
+        sizes: "any",
+      },
+    ],
+  },
 };
