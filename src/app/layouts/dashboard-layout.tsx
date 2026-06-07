4452d31a12ca3bff1430d4bcf5ddfb4bd3c7df5b
518luck <165388484+518luck@users.noreply.github.com>
2026-06-07 21:50:47 +0800

    fix: 恢复提示提供器

--- src/app/layouts/dashboard-layout.tsx
+++ src/app/layouts/dashboard-layout.tsx
@@ -1,3 +1,4 @@
+import { TooltipProvider } from "@/shared/ui/tooltip";
 import { DualSidebarLayout } from "@/widgets/dual-sidebar";
 import { KBar } from "../providers/KBar";
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
@@ -12,9 +13,11 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
           </SidebarInset>
         </SidebarProvider>
       </TooltipProvider> */}
-      <DualSidebarLayout defaultSidebarOpen={false}>
-        {children}
-      </DualSidebarLayout>
+      <TooltipProvider>
+        <DualSidebarLayout defaultSidebarOpen={false}>
+          {children}
+        </DualSidebarLayout>
+      </TooltipProvider>
     </KBar>
   );
 }
